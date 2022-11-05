69ea2d4f1fe48aceb5078cfa5d4e590e3364bb26
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2022-11-05 02:31:22 +0100

    Improve the dash app to be able to plot all x versus all y

--- sensitivity.py
+++ sensitivity.py
@@ -6,6 +6,7 @@ from dash import html
 from dash import dash_table
 from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
+import plotly.express as px
 
 from critical_demand import run_simulation, RESULTS_COLUMN_NAMES
 from utils import read_input_file
@@ -139,6 +140,30 @@ if __name__ == "__main__":
         system_sa_results = pd.read_csv("system_sa_results.csv")
         assets_sa_results = pd.read_csv("assets_sa_results.csv")
 
+    sa_columns = [
+        "sa_input_variable_category",
+        "sa_input_variable_name",
+        "sa_input_variable_value",
+    ]
+
+    categories = system_sa_results.sa_input_variable_category.unique().tolist()
+
+    system_variables = system_sa_results.sa_input_variable_name.unique().tolist()
+    system_output_variables = system_sa_results.param.unique().tolist()
+
+    initial_category = categories[0]
+    assets_variables = (
+        assets_sa_results.loc[
+            assets_sa_results.sa_input_variable_category == initial_category
+        ]
+        .sa_input_variable_name.unique()
+        .tolist()
+    )
+
+    assets_output_categories = assets_sa_results.asset.unique().tolist()
+    assets_output_variables = assets_sa_results.columns.difference(
+        sa_columns + ["asset"]
+    ).tolist()
     # loading external resources
     external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
     options = dict(
@@ -149,21 +174,144 @@ if __name__ == "__main__":
 
     demo_app.layout = html.Div(
         children=[
-            html.H3("Sensitivity analysis outputs"),
-            html.Div(
-                children=dash_table.DataTable(
-                    system_sa_results.to_dict("records"),
-                    [{"name": i, "id": i} for i in system_sa_results.columns],
-                )
+            html.H1("Sensitivity analysis results"),
+            html.P(
+                "Select a category and parameter from inputs of sensitivity analysis (x-axis) and a category and variable from outputs of sensitivity analysis to update the graph below"
             ),
-            html.Div(
-                children=dash_table.DataTable(
-                    assets_sa_results.to_dict("records"),
-                    [{"name": i, "id": i} for i in assets_sa_results.columns],
-                )
+            html.H3("Input parameter (x axis)"),
+            html.Label(htmlFor="input_sa_category_dropdown", children="Category"),
+            dcc.Dropdown(
+                id="input_sa_category_dropdown",
+                options={v: v for v in categories},
+                value=categories[0],
             ),
+            html.Label(htmlFor="input_sa_variable_dropdown", children="Parameter name"),
+            dcc.Dropdown(
+                id="input_sa_variable_dropdown",
+                options={v: v for v in assets_variables},
+                value=assets_variables[0],
+            ),
+            html.H3("Output variable (y axis)"),
+            html.Label(htmlFor="output_sa_category_dropdown", children="Category"),
+            dcc.Dropdown(
+                id="output_sa_category_dropdown",
+                options={v: v for v in ["kpi"] + assets_output_categories},
+                value=assets_output_categories[0],
+            ),
+            html.Label(htmlFor="output_sa_variable_dropdown", children="Variable name"),
+            dcc.Dropdown(
+                id="output_sa_variable_dropdown",
+                options={v: v for v in assets_output_variables},
+                value=assets_output_variables[0],
+            ),
+            html.Div(id="graph-area"),
+            # html.Div(
+            #     children=dash_table.DataTable(
+            #         system_sa_results.to_dict("records"),
+            #         [{"name": i, "id": i} for i in system_sa_results.columns],
+            #     )
+            # ),
+            # html.Div(
+            #     children=dash_table.DataTable(
+            #         assets_sa_results.to_dict("records"),
+            #         [{"name": i, "id": i} for i in assets_sa_results.columns],
+            #     )
+            # ),
         ]
-        # + [dcc.Graph(id="sankey_aggregate", figure=sankey(energy_system, results))]
     )
+
+    @demo_app.callback(
+        # The value of these components of the layout will be changed by this callback
+        [
+            Output(
+                component_id="input_sa_variable_dropdown", component_property="value"
+            ),
+            Output(
+                component_id="input_sa_variable_dropdown", component_property="options"
+            ),
+        ],
+        # Triggers the callback when the value of one of these components of the layout is changed
+        Input(component_id="input_sa_category_dropdown", component_property="value"),
+    )
+    def change_category_value(category):
+        variables = (
+            assets_sa_results.loc[
+                assets_sa_results.sa_input_variable_category == category
+            ]
+            .sa_input_variable_name.unique()
+            .tolist()
+        )
+        return variables[0], {v: v for v in variables}
+
+    @demo_app.callback(
+        # The value of these components of the layout will be changed by this callback
+        [
+            Output(
+                component_id="output_sa_variable_dropdown", component_property="value"
+            ),
+            Output(
+                component_id="output_sa_variable_dropdown", component_property="options"
+            ),
+        ],
+        # Triggers the callback when the value of one of these components of the layout is changed
+        Input(component_id="output_sa_category_dropdown", component_property="value"),
+    )
+    def change_output_category_value(output_category):
+        if output_category == "kpi":
+            variables = system_output_variables
+        else:
+            variables = assets_output_variables
+        return variables[0], {v: v for v in variables}
+
+    @demo_app.callback(
+        # The value of these components of the layout will be changed by this callback
+        Output(component_id="graph-area", component_property="children"),
+        # Triggers the callback when the value of one of these components of the layout is changed
+        [
+            Input(
+                component_id="input_sa_variable_dropdown", component_property="value"
+            ),
+            Input(
+                component_id="output_sa_variable_dropdown", component_property="value",
+            ),
+            Input(
+                component_id="output_sa_category_dropdown", component_property="value",
+            ),
+        ],
+        [State(component_id="input_sa_category_dropdown", component_property="value"),],
+    )
+    def change_asset_variable_value(
+        var_name, output_var_name, output_category, category
+    ):
+
+        x_label = f"{var_name.title()} of {category.title()}".replace("_", " ")
+        y_label = f"{output_var_name.title()} of {output_category.title()}".replace(
+            "_", " "
+        )
+
+        if output_category == "kpi":
+            df = system_sa_results
+            df = df.loc[
+                (df.sa_input_variable_category == category)
+                & (df.sa_input_variable_name == var_name)
+                & (df.param == output_var_name)
+            ]
+            df = df.rename(
+                columns={"value": y_label, "sa_input_variable_value": x_label},
+                # inplace=True,
+            )
+        else:
+            df = assets_sa_results
+            df = df.loc[
+                (df.asset == output_category)
+                & (df.sa_input_variable_name == var_name)
+                # [output_var_name] + sa_columns,
+            ]
+
+            df = df.rename(
+                columns={output_var_name: y_label, "sa_input_variable_value": x_label},
+                # inplace=True,
+            )
+        return dcc.Graph(figure=px.scatter(df, x=x_label, y=y_label))
 
     demo_app.run_server(debug=True, port=8050)
